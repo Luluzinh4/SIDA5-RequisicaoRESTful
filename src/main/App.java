e55b58d5abd0497331ceadd235d2910698b10a7b
Luana Tavares Faiotto <luana.faiotto@gmail.com>
2020-10-21 21:06:58 -0300

    inserção de menu para realizar o request

--- src/main/App.java
+++ src/main/App.java
@@ -10,16 +10,65 @@
 
 package main;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
 		
-		Request reqGet = new Request("https://jsonplaceholder.typicode.com/users/4");
+		Scanner scanner = new Scanner(System.in);
+		
+		byte optionResource = 0;
+		byte optionId = 0;
+		Request reqGet = null;
+		
+		//Menu para selecionar recursos
+		System.out.println("===========================");
+		System.out.println("Selecione qual dos recursos");
+		System.out.println("   você deseja procurar:   ");
+		System.out.println("---------------------------");
+		System.out.println("        1 para posts       ");
+		System.out.println("      2 para comments      ");
+		System.out.println("       3 para albums       ");
+		System.out.println("       4 para photos       ");
+		System.out.println("        5 para todos       ");
+		System.out.println("        6 para users       ");
+		System.out.println("===========================\n");
+		
+		System.out.print("Opção: ");
+		optionResource = scanner.nextByte();
+		
+		System.out.print("Digite o ID que deseja visualizar: ");
+		optionId = scanner.nextByte();
+		
+		switch(optionResource) {
+			case 1:
+				reqGet = new Request("https://jsonplaceholder.typicode.com/posts/" + optionId);
+				break;
+			case 2:
+				reqGet = new Request("https://jsonplaceholder.typicode.com/comments/" + optionId);
+				break;
+			case 3:
+				reqGet = new Request("https://jsonplaceholder.typicode.com/albums/" + optionId);
+				break;
+			case 4:
+				reqGet = new Request("https://jsonplaceholder.typicode.com/photos/" + optionId);
+				break;
+			case 5:
+				reqGet = new Request("https://jsonplaceholder.typicode.com/todos/" + optionId);
+				break;
+			case 6:
+				reqGet = new Request("https://jsonplaceholder.typicode.com/users/" + optionId);
+				break;
+			default:
+				System.out.println("Comando Inválido!");
+				break;
+		}
+		
 		reqGet.requestGet();
 		
-//		Request reqPost = new Request("https://jsonplaceholder.typicode.com/posts");
-//		reqPost.requestPost(4);
-		
+		scanner.close();
+
 	}
 
 }
--- src/main/Request.java
+++ src/main/Request.java
@@ -12,17 +12,14 @@ package main;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Request {
-	
 	private String url;
-	private String resposta = "";
-	
+
 	//Construtor
 	public Request(String url) {
 		this.url = url;
@@ -45,77 +42,15 @@ public class Request {
             BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
 
             while ((linha = br.readLine()) != null) {
-                resposta += linha;
+            	System.out.println(linha);
             }
 
-            System.out.println("Status 200 - OK");
-            System.out.println(resposta);
+//            System.out.println("Status 200 - OK");
             
             conn.disconnect();
         } catch (IOException ex) {
             Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
         }
-	}
-	
-	public void requestPost(int numero) {
-		try {
-//			String ehPrimo = "Não";
-//			if (ehPrimo(numero)) {
-//				ehPrimo = "Sim";
-//			}
-
-	        HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
-	        conn.setRequestMethod("POST");
-	        conn.setRequestProperty("Context-type", "application/json");
-	        conn.setRequestProperty("Accept", "application/json");
-	        conn.setDoOutput(true);
-	        
-	        //String jsonInputString = "{\"Name\": \"Requisição Metodo POST\","
-	        //						+ " \"ValorInserido\": " + numero + ","
-	        //						+ " \"NumeroPrimo\": " + ehPrimo + "}";
-	        
-	        String json = "{userId: 3,"
-	        		   	 + "title: Teste Metodo POST,"
-	        		   	 + "body: Teste XPTO}";
-
-//	        try(OutputStream os = conn.getOutputStream()) {
-//                byte[] input = jsonInputString.getBytes("utf-8");
-//                os.write(input, 0, input.length);
-//            }catch (Exception e){
-//                e.printStackTrace();
-//            }
-	        try(OutputStream os = conn.getOutputStream()) {
-                byte[] input = json.getBytes("utf-8");
-                os.write(input, 0, input.length);
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-
-            try(BufferedReader br = new BufferedReader(
-                    new InputStreamReader(conn.getInputStream()))) {
-                StringBuilder response = new StringBuilder();
-                String responseLine = null;
-                while ((responseLine = br.readLine()) != null) {
-                    response.append(responseLine.trim());
-                }
-                System.out.println(response.toString());
-            } catch(Exception e) {
-            	e.printStackTrace();
-            }
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("Erro no método POST");
-		}
-	}
-	
-	
-	//Métodos Auxiliares
-	public boolean ehPrimo(int numero) {
-	    for (int i = 2; i < numero; i++) {
-	        if (numero % i == 0)
-	            return false;   
-	    }
-	    return true;
 	}
 
 	
@@ -126,14 +61,6 @@ public class Request {
 
 	public void setUrl(String url) {
 		this.url = url;
-	}
-
-	public String getResponse() {
-		return resposta;
-	}
-
-	public void setResponse(String response) {
-		this.resposta = response;
 	}
 	
 }
